1fc6eb875472258fefa56fd3af5c3758a6e1650c
weavelol <deniss.shutov98@gmail.com>
2022-01-16 21:15:59 +0200

    Abstract Model

---
+++ Models/AbstractModel.cs
@@ -0,0 +1,6 @@
+﻿namespace Models {
+    public class AbstractModel {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
--- Models/Operation.cs
+++ Models/Operation.cs
@@ -1,14 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Models {
-    public class Operation {
-        public int Id { get; set; }
+    public class Operation : AbstractModel {
         public int OperationTypeId { get; set; }
-        public string Name { get; set; }
         public DateTime Date { get; set; }
         public double Amount { get; set; }
     }
